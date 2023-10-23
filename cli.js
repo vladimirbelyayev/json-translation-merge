0887212f528b074efc5889082f48d84b03f1178c
Vladimir Belyayev <vladimirs.belajevs@gmail.com>
2023-10-23 13:01:04 +0300

    Added yarg parameter processing

--- cli.js
+++ cli.js
@@ -1,5 +1,34 @@
 #! /usr/bin/env node
 
+var fs = require("fs");
+const yargs = require("yargs");
 const mergeFiles = require("./main");
 
-mergeFiles(process.argv[2], process.argv[3]);
+const coercePath = (path) => {
+  if (!fs.lstatSync(path).isDirectory()) {
+    throw new Error(`"${path}" is not a valid path`);
+  }
+  return path;
+};
+
+const builder = (command) =>
+  command
+    .positional("source", {
+      describe: "source path from which to aggregate translation json files",
+      type: "string",
+      coerce: coercePath,
+    })
+    .positional("destination", {
+      describe:
+        "destination path whre to place the aggregated translation files",
+      type: "string",
+      coerce: coercePath,
+    });
+
+const handler = ({ source, destination }) => mergeFiles(source, destination);
+
+yargs
+  .command("$0 <source> <destination>", false, builder, handler)
+  .help()
+  .wrap(yargs.terminalWidth())
+  .parse();
